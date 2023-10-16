6446fd29177e6f0afd5d4006ea4c742ef5031a08
Patricia Costa <patricosouza@hotmail.com>
2023-10-16 01:17:23 -0300

    teste

--- src/components/campanha/campaignChart.tsx
+++ src/components/campanha/campaignChart.tsx
@@ -133,7 +133,7 @@ export function CampaignChart() {
                     </Stat>
                 </Box>
                 <Box bgColor='gray.50' w='8px' h='8px' borderRadius='100%' />
-                <Box textAlign='center' bgColor='gray.50' px={{base: 8, lg: 12}} py={{base: 2, lg: 8}}>
+                <Box textAlign='center' bgColor='gray.50' px={{base: 8, lg: 12}} py={{base: 2, lg: 8}} h='100%'>
                     <Box>
                         <Text fontWeight='bold' fontSize={{base: 28, lg: 28}} color='blue.800'>2020</Text>
                     </Box>
@@ -154,6 +154,7 @@ export function CampaignChart() {
                 justifyContent={{base: 'space-between', lg: 'center'}} 
                 w={{base: '100%', lg: 'fit-content'}}
                 gap={{base: 4, lg: 8}}
+                mt={{base: 8, lg: 0}}
             >
                 <Box bgColor='gray.50' w='8px' h='8px' borderRadius='100%' display={{base: 'none', lg: 'block'}} />
                 <Box textAlign='center' bgColor='gray.50' px={{base: 8, lg: 12}} py={{base: 2, lg: 8}}>
