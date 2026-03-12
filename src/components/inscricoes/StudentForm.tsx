10837578f4353aec74318680ad74fa777f1cc4a2
chouzzy <mathfernando@hotmail.com>
2026-03-11 21:46:35 -0300

    termo de inscrição correto

--- src/components/inscricoes/StudentForm.tsx
+++ src/components/inscricoes/StudentForm.tsx
@@ -479,7 +479,7 @@ export function StudentForm({ selectedTurma, onBack, onSubmit, isSubmitting }: S
               id="aceiteTermoCiencia"
               {...register('aceiteTermoCiencia', { required: 'Você deve aceitar o Termo de Ciência' })}
             >
-              Eu li e aceito o <Link href="https://drive.google.com/file/d/1uRIViYS0y9Ji1QvbPcN12_mOpcisjxD1/view?usp=sharing" isExternal color="blue.500">Termo de Ciência</Link> (Obrigatório)
+              Eu li e aceito o <Link href={'https://drive.google.com/file/d/17KFQVgR8v46ArvdaCUuwgfxs_Q1RcaN9/view'} isExternal color="blue.500">Termo de Ciência</Link> (Obrigatório)
             </Checkbox>
             <FormErrorMessage>{errors.aceiteTermoCiencia && String(errors.aceiteTermoCiencia.message)}</FormErrorMessage>
             <Text fontSize="xs" color="gray.500" mt={1}>
@@ -492,7 +492,8 @@ export function StudentForm({ selectedTurma, onBack, onSubmit, isSubmitting }: S
               id="aceiteTermoInscricao"
               {...register('aceiteTermoInscricao', { required: 'Você deve aceitar o Termo de Inscrição' })}
             >
-              Eu li e aceito o <Link href="https://drive.google.com/file/d/1fHvtwtkWgH0RLD5lyw4TLRdQ9pEfGBui/view?usp=sharing" isExternal color="blue.500">
+              Eu li e aceito o <Link href={selectedTurma.documents?.find(doc => doc.title === 'Termo de Inscrição')?.downloadLink} isExternal color="blue.500">
+                
                 Termo de Inscrição <Icon as={ExternalLinkIcon} mx="2px" />
               </Link> (Obrigatório)
             </Checkbox>
